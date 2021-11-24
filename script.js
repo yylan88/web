9a158482e7fcdc09630c4dfc6bf387327866d826
yylan88 <landy680112@gmail.com>
2021-11-25 02:01:20 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+            //  展開/收合按鈕
+            $(document).ready(function() {
+            
+                $("#collapse").on("click", function() {
+            
+                    $("#sidebar").toggleClass("active");
+                    $(".fa-align-left").toggleClass("fa-chevron-circle-right");
+                })
+            })
+
+      
